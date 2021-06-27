ee4675e5e125c6c5e5467780aea7c2df9cca44d7
malanchristiansen <malanschristiansen@gmail.com>
2021-06-27 15:49:01 +1000

    completed forms

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,19 +3,28 @@ class MoviesController < ApplicationController
     end
 
     def create
+        #puts params 
+        if session[:movies] == nil 
+            session[:movies] = []
+        end 
+        session[:movies].push(params[:movie])
+        redirect_to movie_path(session[:movies].length)
     end
 
     def new
     end
 
     def show
+        # exmaple of what's going in code below: session[:movies] = ["Avengers", "the dark knight"]
+        @movie = session[:movies][params[:id].to_i - 1]
     end
 
     def edit
-       
+        @movie = session[:movies][params[:id].to_i - 1]
     end
 
     def update
-        
+        session[:movies][params[:id].to_i - 1] = params[:movie]
+        redirect_to movie_path(params[:id].to_i)
     end
 end
